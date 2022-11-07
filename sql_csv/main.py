8e5affc685a69b834a5706c381003e2f6e7e1ee2
Adishumla <45212964+Adishumla@users.noreply.github.com>
2022-11-07 18:23:17 +0100

    added new csv files

--- sql_csv/main.py
+++ sql_csv/main.py
@@ -2,9 +2,6 @@ import pandas as pd
 
 df = pd.read_csv("../csv/cards.csv")
 
-# update card_type column with yellow randomly to 15% of the rows
-df.loc[df.sample(frac=0.15).index, "card_type"] = "yellow"
-# update card_type column with red randomly to 7% of the rows
-df.loc[df.sample(frac=0.07).index, "card_type"] = "red"
+# update 
 
 df.to_csv("../csv/cards.csv", index=False)
